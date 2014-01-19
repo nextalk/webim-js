35a0daae9d462677b9a51ded149d97254609dc85
Arron <arronzhang@me.com>
2014-01-19 22:26:51 +0800

    Fix msg

--- test/data/message.php
+++ test/data/message.php
@@ -1,7 +1,7 @@
 <?php
 include_once("common.php");
 if($connection["ticket"] == gp("ticket")){
-	echo callback("ok");
+	echo callback(array("message" => "禁止关键词xxx"));
 }else{
 	header("HTTP/1.0 404 Not Found");
 	echo "No client.";
